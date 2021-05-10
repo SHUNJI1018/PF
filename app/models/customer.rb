d1326d675c40b2884cc5dcd6889050c3c5f9b4d2
SHUNJI1018 <hide971gg@gmail.com>
2021-05-10 10:59:14 +0000

    [Add&Update] sign_in,sign_up画面の編集

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -6,4 +6,14 @@ class Customer < ApplicationRecord
   
   has_many :diys, dependent: :destroy
   
+  # ログイン時の空白を許可しない
+  validates :nickname, :last_name, :first_name, :last_kana_name, :first_kana_name, presence: true
+  
+  # サインイン時のカタカナ入力に関するバリデーション
+  validates :last_kana_name, :first_kana_name,
+            format: { with: /\A[\p{katakana}　ー－&&[^ -~｡-ﾟ]]+\z/, message: "全角カタカナのみで入力して下さい" }
+  
+  # ユーザーステータスの二択
+  enum is_valid: { 有効: false, 退会: true }
+  
 end

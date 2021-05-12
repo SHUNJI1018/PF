325927d771cf15e8c7f2206e5cbea6961e4bbf45
SHUNJI1018 <hide971gg@gmail.com>
2021-05-12 05:40:46 +0000

    [Add] favorite

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,7 @@
 class Public::HomesController < ApplicationController
   
   def top
+    @diys = Diy.all
   end
   
   def about
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_040944) do
+ActiveRecord::Schema.define(version: 2021_05_12_031842) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 2021_05_10_040944) do
     t.string "diy_name"
     t.string "image_id"
     t.text "explanation"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "customer_id"
+    t.integer "diy_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
